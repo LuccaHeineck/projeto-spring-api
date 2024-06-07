c110d8e343a23c409c32c5029f3258df625f8042
Lucca <luccacheineck@gmail.com>
2024-06-07 19:35:34 -0300

    first commit

---
+++ src/main/java/com/example/projetospringapi/ProjetoSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetospringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSpringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projetospringapi/ProjetoSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetospringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
